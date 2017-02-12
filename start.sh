0372f4a92ed75d28dc9e65bbfbe4c1c9b4d30bb7
Tangcheng <helloworld.tang@qq.com>
2017-02-12 12:17:34 +0800

    bak

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-nohup java -jar -Dspring.profiles.active=prod  selectdata-0.0.1-SNAPSHOT.jar 2>error.log 1>/dev/null &
+nohup java -jar -Dspring.profiles.active=prod,swagger -Dserver.port=7812 selectdata-0.0.1-SNAPSHOT.jar 2>error.log 1>/dev/null &
 
 
 
