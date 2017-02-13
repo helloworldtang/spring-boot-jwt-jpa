a25940c4d1f281b76ffc755e601770e9cd461791
Tangcheng <helloworld.tang@qq.com>
2017-02-14 00:02:19 +0800

    fix bugs

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -46,7 +46,7 @@ CREATE TABLE `tb_daily_category` (
 drop TABLE if EXISTS tb_daily_news;
 CREATE TABLE `tb_daily_news` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `category_id` smallint(1) DEFAULT NULL,
+  `category_id` smallint(1) NOT NULL,
   `title` varchar(50) NOT NULL,
   `media_url` varchar(100) NOT NULL,
   `source` varchar(50) NOT NULL,
@@ -55,8 +55,8 @@ CREATE TABLE `tb_daily_news` (
   `updated_time` datetime DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
--- ALTER TABLE `tb_daily_news`
--- ADD COLUMN `categoryId`  smallint(1) NULL AFTER `id`;
+ALTER TABLE `tb_daily_news`
+ADD COLUMN `category_id`  smallint(1) NOT NULL AFTER `id`;
 INSERT INTO `tb_daily_news` (`id`, `title`, `media_url`, `source`, `status`, `created_time`, `updated_time`) VALUES ('1', '揭秘比特币和区块链（五）：深入理解比特币交易的脚本', 'http://www.infoq.com/cn/articles/deep-understanding-of-bitcoin-transaction-script', 'InfoQ', '0', '2017-01-15 19:08:41', '2017-01-15 19:08:41');
 INSERT INTO `tb_daily_news` (`id`, `title`, `media_url`, `source`, `status`, `created_time`, `updated_time`) VALUES ('2', '阿里云、Amazon、Google云数据库方案架构与技术分析', 'http://www.infoq.com/cn/articles/cloud-database-schema-and-technical-analysis', 'InfoQ', '0', '2017-01-15 19:10:41', '2017-01-15 19:10:41');
 INSERT INTO `tb_daily_news` (`id`, `title`, `media_url`, `source`, `status`, `created_time`, `updated_time`) VALUES ('3', '大数据挖掘更多时间都在于清洗数据', 'http://www.infoq.com/cn/articles/more-time-of-big-data-mining-is-used-to-clean-the-data', 'InfoQ', '0', '2017-01-15 19:15:41', '2017-01-15 19:15:41');
